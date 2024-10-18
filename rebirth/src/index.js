b5eb3c7762e4953512f067b88b1406c6587a36cb
SpeedyNAS <naxelalejandro@hotmail.com>
2024-10-18 17:50:08 -0500

    Adding style templates
    Added some template styles for use

--- rebirth/src/index.js
+++ rebirth/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/index.css';
 import './styles/style.css';
+import './styles/utils.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
