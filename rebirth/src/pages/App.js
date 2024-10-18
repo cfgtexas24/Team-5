6ffa5f16bab426efff4b59b960d92bf998fab6e5
SpeedyNAS <naxelalejandro@hotmail.com>
2024-10-18 17:30:31 -0500

    Update App.js

--- rebirth/src/pages/App.js
+++ rebirth/src/pages/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='App'>
-        App
+        <h1 className='text-2xl text-red-700'>Rebirth App is Live!</h1>
     </div>
   )
 }
