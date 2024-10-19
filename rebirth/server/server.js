e0c094b0456c640154487a347c276ac9ab03b486
SpeedyNAS <naxelalejandro@hotmail.com>
2024-10-19 02:06:57 -0500

    Database is connected and working!!!!!!!!!!!!!!!!!!!!
    FIxed CORS policy to actually be able to connect the database

--- rebirth/server/server.js
+++ rebirth/server/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+app.use(cors());
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -10,7 +12,7 @@ app.set('view engine', 'ejs')
 app.get('/', logger, (req, res) => {
     console.log('Here')
     //res.download("server.js") // download a file
-    res.render('index', { text: 'World' }) // render a view
+    // res.render('index', { text: 'World' }) // render a view
     //res.status(500).json({ message: 'Error' })
     //res.send('Hi') // send mainly used for testing purposes
 })
--- rebirth/src/pages/Home.js
+++ rebirth/src/pages/Home.js
@@ -2,18 +2,20 @@ import React from 'react'
 import Navigation from '../components/Navigation';
 import { Outlet } from 'react-router';
 import ProfileLink from '../components/ProfileLink';
+import UsersComponent from '../components/test';
 
 const Home = () => {
-  return (
-    <div className='App'>
-        <Navigation />
-        <div className='ml-[16.6vw] px-16 py-8'>
-            <Outlet />
-        </div>
+    return (
+        <div className='App'>
+            <Navigation />
+            <div className='ml-[16.6vw] px-16 py-8'>
+                <UsersComponent />
+                <Outlet />
+            </div>
 
-        <ProfileLink />
-    </div>
-  )
+            <ProfileLink />
+        </div>
+    )
 }
 
 export default Home;
