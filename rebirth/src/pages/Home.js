de075352ab2667f6bb582c7414f79d456d0e9328
SpeedyNAS <naxelalejandro@hotmail.com>
2024-10-19 04:01:03 -0500

    Moved MyApplications

--- rebirth/src/pages/Home.js
+++ rebirth/src/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Navigation from '../components/Navigation';
 import { Outlet } from 'react-router';
 import ProfileLink from '../components/ProfileLink';
-import UsersComponent from '../components/test';
+// import UsersComponent from '../components/test';
 
 const Home = () => {
     return (
         <div className='App'>
             <Navigation />
             <div className='ml-[16.6vw] px-16 py-8'>
-                <UsersComponent />
+                {/* <UsersComponent /> */}
                 <Outlet />
             </div>
 
