5ac74f591acee7c0af9216d125abc1462aa6a6c7
Emma Pham <emmauyentrinh@gmail.com>
2024-10-19 03:35:54 -0500

    MOVED APPLICATIONS

--- rebirth/src/pages/user/MyApplication.js
+++ rebirth/src/pages/user/MyApplication.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from 'react';
 
 const MyApplication = () => {
   return (
-    <div>MyApplication</div>
-  )
-}
+    <div style={{ width: "100%", marginTop: "150px" }}>
+      <h3>My Applications</h3>
+      <div style={{ maxHeight: "200px", overflowY: "scroll", border: "1px solid #ccc", padding: "10px" }}>
+        <ul className="flex flex-col gap-4">
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+          <li>
+            <div style={{ background: "#f0f0f0", borderRadius: "8px", padding: "10px" }}>
+              Job Title
+              <p>Description</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default MyApplication;
