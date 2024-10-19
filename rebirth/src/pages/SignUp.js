3cb68539a1fab43821d99f40187549689f6ad690
emmastubby <98038103+emmastubby@users.noreply.github.com>
2024-10-19 07:45:45 -0500

    rename signup to login page

--- rebirth/src/pages/SignUp.js
+++ rebirth/src/pages/SignUp.js
@@ -6,16 +6,16 @@ const Signup = () => {
     return (
         <div className="signup-container">
             <div className="signup-box">
-                <div className="logo">   
+                <div className="logo mb-4">   
                     <img src="https://rebirthempowermenteducation.org/wp-content/uploads/2017/09/Screenshot-2023-06-24-at-11.17.24-AM.png" alt="Rebirth Empowerment Logo" className="logo-image" />
                 </div>
-                <h2>Sign Up</h2>
+                <h2>Login</h2>
                 <p>To get started</p>
                 <form>
-                <div className="input-group">
+                {/* <div className="input-group">
                     <label htmlFor="name">Name</label>
                     <input type="text" id="name" placeholder="Enter your name" />
-                </div>
+                </div> */}
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" placeholder="Enter your email" />
