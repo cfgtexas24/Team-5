a9e85e5903acf5f48853c437e44faf454402c9d4
SpeedyNAS <naxelalejandro@hotmail.com>
2024-10-18 19:17:24 -0500

    Routes Setup
    Setup the routes for all the pages

--- rebirth/src/index.js
+++ rebirth/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import './styles/style.css';
 import './styles/utils.css';
-import App from './pages/App';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+import Dashboard from './pages/Dashboard';
+import JobBoard from './pages/JobBoard';
+import MyLearning from './pages/MyLearning';
+import Profile from './pages/Profile';
+import DiscussionBoard from './pages/DiscussionBoard';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path='/job-board' element={<JobBoard />} />
+                    <Route path='/mylearning' element={<MyLearning />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/discussion-board' element={<DiscussionBoard />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ rebirth/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../components/Navigation';
+import { Outlet } from 'react-router';
+
+const Home = () => {
+  return (
+    <div className='App'>
+        <Navigation />
+        <h1 className='text-2xl text-red-700'>Rebirth App is Live!</h1>
+        <Outlet />
+    </div>
+  )
+}
+
+export default Home;
